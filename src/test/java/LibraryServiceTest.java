baa7856b05acc74cc5e137e9f6267b2e0a4eb504
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2025-02-28 20:17:48 -0500

    Se edita método getBookById para que solo llame una vez al método del BookRepository y se hacen pruebas unitarias del métodos, las cuales salen satisfactorias

--- src/test/java/LibraryServiceTest.java
+++ src/test/java/LibraryServiceTest.java
@@ -1,11 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
 import org.mockito.MockitoAnnotations;
 
 import com.devsenior.nmanja.controller.LibraryService;
 import com.devsenior.nmanja.interfaces.BookRepository;
 import com.devsenior.nmanja.interfaces.LoanRepository;
+import com.devsenior.nmanja.model.Book;
 
 public class LibraryServiceTest {
 
@@ -29,11 +35,38 @@ public class LibraryServiceTest {
         var title = "Libro 1";
         var author = "Autor 1";
 
+        
+
         libraryService.addBook(id, title, author);
+
+        verify(bookRepository).saveBook(any(Book.class)); //Se verifica que se haya llamado al metodo y que si agrega un objeto de tipo libro
+
+        
+
         
     }
 
-    
+
+    @Test
+
+    void testGetBookById() {
+
+        var mockBook = new Book("111","El principito","Antoine de Saint-Exupery");
+
+        Mockito.when(bookRepository.findById("111")).thenReturn(mockBook); //Se mockea el metodo
+        
+
+        Book book = libraryService.getBookById("111"); //Se llama al metodo
+
+        assertNotNull(book,"No se encontró el libro");
+
+        assertEquals(mockBook, book);
+        assertEquals("111", book.getId());
+        assertEquals("El principito", book.getTitle());
+        assertEquals("Antoine de Saint-Exupery", book.getAuthor());
+
+
+    }
 
 
 
